60312f2741d5ba499fcc289a800104551c78fd0f
Brijisha <brijishadoshi12@gmail.com>
2022-11-01 00:44:19 +0530

    Correct overflow issue in li

--- src/Task.js
+++ src/Task.js
@@ -5,7 +5,7 @@ const Task = (props) => {
 
   return (
     <div className="row mx-auto">
-     <li className="shadow p-3 my-2 col-sm-9 fw-bold">{props.value}</li>
+     <li className="shadow p-3 my-2 col-sm-9 fw-bold text-wrap">{props.value}</li>
      <button type="button" className="btn border btn-danger col-2 col-sm-2 my-2 mx-4" onClick={()=>{props.deleteTask(props.id)}}>X</button>
        
     </div>
